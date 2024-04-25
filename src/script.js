7d0967f32bb69a82f59593d6721ad1a156b8eca9
Laura Purcell-Gates <l.purcellgates@gmail.com>
2024-04-25 16:58:55 +0100

    frame built

---
+++ src/script.js
@@ -0,0 +1,71 @@
+const checkStatus = (response) => {
+  if (response.ok) {
+    // .ok returns true if response status is 200-299
+    return response;
+  }
+  throw new Error('Request was either a 404 or 500');
+}
+
+const json = (response) => response.json()
+
+// construct class, initialise new_task, create empty tasks array
+class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      new_task: '',
+      tasks: [],
+    };
+
+    // bind functions
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // create handleChange function
+  handleChange(event) {
+    // set state of relevant new_task to input value
+    this.setState({ new_task: event.target.value });
+  }
+
+  // create handleSubmit function
+  handleSubmit(event) {
+    // for now, do nothing
+    event.preventDefault();
+  }
+
+  // create render function
+  render() {
+    const { new_task, tasks } = this.state;
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h2 className="mb-3">To Do List</h2>
+            {// use conditional operator
+            tasks.length > 0 ? tasks.map((task) => {
+              // for now, return nothing
+              return null;
+            }) : <p>no tasks here</p>}
+            <form onSubmit={this.handleSubmit} className="form-inline my-4">
+              <input 
+                type="text"
+                className="form-control mr-sm-2 mb-2"
+                placeholder="new task"
+                value={new_task}
+                onChange={this.handleChange}
+                />
+                <button type="submit" className="btn btn-primary mb-2">Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+// render into DOM
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<ToDoList />);
